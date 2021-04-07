b75ce4bbeb0ca5fb2ee53339f032b3e1a04fa4f1
Daniel Schmidt Pinheiro <3842788+dscpinheiro@users.noreply.github.com>
2021-04-07 19:57:58 -0400

    Add CDK application for backup (using S3)

---
+++ s3-backup-cdk/README.md
@@ -0,0 +1,13 @@
+This TypeScript project that creates the following resources:
+ * An Amazon S3 bucket to be used for backups
+ * An AWS KMS key to encrypt the bucket
+
+The `cdk.json` file tells the CDK Toolkit how to execute your app.
+
+## Useful commands
+
+ * `npm run build`   compile typescript to js
+ * `npm run watch`   watch for changes and compile
+ * `cdk deploy`      deploy this stack to your default AWS account/region
+ * `cdk diff`        compare deployed stack with current state
+ * `cdk synth`       emits the synthesized CloudFormation template
---
+++ s3-backup-cdk/lib/bkp-stack.ts
@@ -0,0 +1,60 @@
+import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
+import * as kms from '@aws-cdk/aws-kms';
+import * as iam from '@aws-cdk/aws-iam';
+
+export class BackupStack extends cdk.Stack {
+    constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
+
+        const keyAdmin = iam.User.fromUserName(this, 'BackupKeyAdmin', 'keyadmin');
+
+        const backupKey = new kms.Key(this, 'S3BackupKey', {
+            admins: [keyAdmin],
+            alias: 's3-backup-key',
+            description: 'Key to be used for S3 SSE-KMS',
+            enabled: true
+        });
+
+        const backupBucket = new s3.Bucket(this, 'BackupBucket', {
+            blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+            bucketKeyEnabled: true,
+            encryption: s3.BucketEncryption.KMS,
+            encryptionKey: backupKey,
+            lifecycleRules: [
+                {
+                    id: 'BackupLifecycleRule',
+                    enabled: true,
+                    abortIncompleteMultipartUploadAfter: cdk.Duration.days(7),
+                    noncurrentVersionExpiration: cdk.Duration.days(90),
+                    noncurrentVersionTransitions: [
+                        {
+                            storageClass: s3.StorageClass.ONE_ZONE_INFREQUENT_ACCESS,
+                            transitionAfter: cdk.Duration.days(45)
+                        }
+                    ],
+                    transitions: [
+                        {
+                            storageClass: s3.StorageClass.INFREQUENT_ACCESS,
+                            transitionAfter: cdk.Duration.days(60)
+                        },
+                        {
+                            storageClass: s3.StorageClass.GLACIER,
+                            transitionAfter: cdk.Duration.days(365)
+                        }
+                    ]
+                }
+            ],
+            objectOwnership: s3.ObjectOwnership.BUCKET_OWNER_PREFERRED,
+            versioned: true
+        });
+
+        new cdk.CfnOutput(this, 'KmsKeyArn', {
+            value: backupKey.keyArn
+        });
+
+        new cdk.CfnOutput(this, 'BackupBucketArn', {
+            value: backupBucket.bucketArn
+        });
+    }
+}
