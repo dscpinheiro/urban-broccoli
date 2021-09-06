54287361d044847ee16de0c46826e51f2d32c575
Daniel Schmidt Pinheiro <3842788+dscpinheiro@users.noreply.github.com>
2021-09-06 16:22:44 -0400

    Refactor transitions to use INTELLIGENT_TIERING

--- s3-backup-cdk/lib/bkp-stack.ts
+++ s3-backup-cdk/lib/bkp-stack.ts
@@ -24,13 +24,7 @@ export class BackupStack extends cdk.Stack {
                     id: 'InventoryLifecycleRule',
                     enabled: true,
                     abortIncompleteMultipartUploadAfter: cdk.Duration.days(7),
-                    expiration: cdk.Duration.days(365),
-                    transitions: [
-                        {
-                            storageClass: s3.StorageClass.INFREQUENT_ACCESS,
-                            transitionAfter: cdk.Duration.days(180)
-                        }
-                    ]
+                    expiration: cdk.Duration.days(60)
                 }
             ]
         });
@@ -45,21 +39,17 @@ export class BackupStack extends cdk.Stack {
                     id: 'BackupLifecycleRule',
                     enabled: true,
                     abortIncompleteMultipartUploadAfter: cdk.Duration.days(7),
-                    noncurrentVersionExpiration: cdk.Duration.days(90),
+                    noncurrentVersionExpiration: cdk.Duration.days(60),
                     noncurrentVersionTransitions: [
                         {
                             storageClass: s3.StorageClass.ONE_ZONE_INFREQUENT_ACCESS,
-                            transitionAfter: cdk.Duration.days(45)
+                            transitionAfter: cdk.Duration.days(30)
                         }
                     ],
                     transitions: [
                         {
-                            storageClass: s3.StorageClass.INFREQUENT_ACCESS,
-                            transitionAfter: cdk.Duration.days(60)
-                        },
-                        {
-                            storageClass: s3.StorageClass.GLACIER,
-                            transitionAfter: cdk.Duration.days(365)
+                            storageClass: s3.StorageClass.INTELLIGENT_TIERING,
+                            transitionAfter: cdk.Duration.days(30)
                         }
                     ]
                 }
